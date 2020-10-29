56466cddab963d901445320aae0661d245537dad
Lenovo <kshitij.maheshwari_cs19@gla.ac.in>
2020-10-29 18:18:58 +0530

    Add constructor and field for Tortoise.java

--- src/thread/tortoise/Tortoise.java
+++ src/thread/tortoise/Tortoise.java
@@ -2,4 +2,11 @@ package thread.tortoise;
 
 public class Tortoise extends Thread {
 
+    private static final int MILESTONES = 5;
+
+    public Tortoise() {
+        super("Tortoise");
+    }
+
+
 }
